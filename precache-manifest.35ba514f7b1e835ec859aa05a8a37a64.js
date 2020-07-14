f9508d36ea5fe412e64e4103945ef9980afb2fd8
William Clark <william_clark@outlook.com>
2020-07-13 23:39:43 -0400

    Updates

---
+++ precache-manifest.35ba514f7b1e835ec859aa05a8a37a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6dee11386e727ad2cc12764da277969",
+    "url": "/UnSatisfactoryFactoryCalculator/index.html"
+  },
+  {
+    "revision": "537cbdafc2e798d7ddc8",
+    "url": "/UnSatisfactoryFactoryCalculator/static/css/main.80a55bc8.chunk.css"
+  },
+  {
+    "revision": "39bee492c7ef4919c599",
+    "url": "/UnSatisfactoryFactoryCalculator/static/js/2.08071ef4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UnSatisfactoryFactoryCalculator/static/js/2.08071ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537cbdafc2e798d7ddc8",
+    "url": "/UnSatisfactoryFactoryCalculator/static/js/main.787298bb.chunk.js"
+  },
+  {
+    "revision": "3c98dabd56d5ca99ab4d",
+    "url": "/UnSatisfactoryFactoryCalculator/static/js/runtime-main.f01affec.js"
+  }
+]);
